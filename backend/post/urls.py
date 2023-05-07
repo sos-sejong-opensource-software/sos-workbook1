4b05e21f62494f1dbe28202d8ed9be35dc368c82
Jimin <50089365+Dingadung@users.noreply.github.com>
2023-05-07 20:42:33 +0900

    Delete backend directory

--- backend/post/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import (ListPostView, DetailPostView)
-
-urlpatterns = [
-   path('', ListPostView.as_view(), name="post_api"),
-   path('<int:pk>/', DetailPostView.as_view(), name="post_detail_api"),
-]
